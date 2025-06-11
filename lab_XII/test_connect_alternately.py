4f32c78ddfa6eb447023a727cbe08e675dedba67
rodakt <tomasz.rodak@wmii.uni.lodz.pl>
2025-06-11 11:28:50 +0200

    lab 12 poprawka

--- lab_XII/test_connect_alternately.py
+++ lab_XII/test_connect_alternately.py
@@ -3,30 +3,6 @@
 import unittest
 from connect_alternately import connect_alternately2, connect_alternately
 
-# assert list(connect_alternately("abc", "xyz")) == list("axbycz")
-# assert list(connect_alternately("abc", "xy")) == list("axbyc")
-# assert list(connect_alternately("ab", "xyz")) == list("axby")
-# assert list(connect_alternately("abc", "")) == ["a"]
-# assert list(connect_alternately("", "xyz")) == []
-# assert list(connect_alternately([1, 2], [3, 4])) == [1, 3, 2, 4]
-
-# it = connect_alternately((1, 2), "a")
-
-# assert next(it) == 1
-# assert next(it) == "a"
-# assert next(it) == 2
-
-# try:
-#     next(it)
-# except StopIteration:
-#     pass
-# else:
-#     raise AssertionError("Generator został wyczerpany")
-# assert list(connect_alternately()) == []
-# assert list(connect_alternately("a", "xy", "uvw")) == list("axu")
-# assert list(connect_alternately("abc", "xy", "u")) == list("axuby")
-# assert list(connect_alternately("abc", "kl", "", "u")) == list("ak")
-
 
 class TestConnectAlternately2(unittest.TestCase):
     """Testy dla funkcji connect_alternately2."""
@@ -98,12 +74,13 @@ class TestConnectAlternately(unittest.TestCase):
 
         it = connect_alternately(infinite1(), infinite2())
         self.assertEqual([next(it) for _ in range(10)], [1, 2] * 5)
-    
+
     def test_many_iterables(self):
         """Testy dla wielu iteratorów."""
         self.assertEqual(list(connect_alternately("a", "xy", "uvw")), list("axu"))
         self.assertEqual(list(connect_alternately("abc", "xy", "u")), list("axuby"))
         self.assertEqual(list(connect_alternately("abc", "kl", "", "u")), list("ak"))
+
 
 if __name__ == "__main__":
     unittest.main()
